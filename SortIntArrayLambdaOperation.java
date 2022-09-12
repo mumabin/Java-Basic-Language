dcbb3a41aaf2379455a398530eaeca26527ee440
mumabin <xiangzhengbin2012@gmail.com>
2022-09-11 18:15:11 -0700

    Update and rename IntArrayLambdaOperation.java to SortIntArrayLambdaOperation.java

--- SortIntArrayLambdaOperation.java
+++ SortIntArrayLambdaOperation.java
@@ -10,7 +10,11 @@ class Solution {
                                             
         Arrays.stream(nums).forEach(num -> map.put(num, map.getOrDefault(num, 0) + 1));
         
-                                     
+        
+        // Convert int[] to Integer[]
+        // Integer[] input = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+
+        
        return  Arrays.stream(nums).boxed().sorted((a, b) -> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : b - a)
             .mapToInt(n -> n)
             .toArray();
